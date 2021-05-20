cff069e9f07ec61c0810ab977a2909a840e87ac5
Zaray <cor191025@uvg.edu.gt>
2021-05-20 02:51:34 -0600

    register

--- frontend/src/components/signUp/signUp.jsx
+++ frontend/src/components/signUp/signUp.jsx
@@ -4,10 +4,11 @@ import img from '../../assets/tutor3.png';
 function SignUp() {
   return (
     <div className="signUp-container">
-      <SignUpFormcontainer />
       <div className="signUp-img">
-         <img src={img} className={'d-none d-md-block vh-100 w-100'} alt="LOGO"/>
+        <img src={img} className={'d-none d-md-block vh-100 w-100'} alt="LOGO"/>
       </div>
+      <SignUpFormcontainer />
+
     </div>
   );
 }
