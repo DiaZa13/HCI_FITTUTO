5496e048201d8597409585c64dafb50830befc34
Zaray <cor191025@uvg.edu.gt>
2021-05-27 07:43:39 -0600

    home

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
-import App from './components/app';
+import App from './components/signUp/signUp';
 
 const container = document.getElementById('root');
 ReactDOM.render(<App />, container);
